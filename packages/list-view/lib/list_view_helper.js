0cf3bf47d7326092244ba56b01d0c97a0e129e4a
Zack Angelo <zackangelo@gmail.com>
2013-07-21 14:09:46 -0500

    Apply correct width/height dimensions when webkit css transform is present

--- packages/list-view/lib/list_view_helper.js
+++ packages/list-view/lib/list_view_helper.js
@@ -7,6 +7,12 @@ Ember.ListViewHelper = {
         var x = position.x,
             y = position.y;
 
+        var w = this.get('elementWidth'),
+            h = this.get('rowHeight'); 
+
+        element.style.width = w + 'px'; 
+        element.style.height = h + 'px';
+
         element.style.webkitTransform = 'translate3d(' + x + 'px, ' + y + 'px, 0)';
       };
     }else{
